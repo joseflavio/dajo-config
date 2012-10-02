e4996c0d00da85019596c6a11239a1afded9e43a
joseflavio <joseflavio@gmail.com>
2012-10-02 18:32:20 +0200

    [fix] small fix

--- dajo-config/dajo-config/src/org/dajo/framework/adapters/TypeAdapterResult.java
+++ dajo-config/dajo-config/src/org/dajo/framework/adapters/TypeAdapterResult.java
@@ -5,12 +5,12 @@ public final class TypeAdapterResult<T> {
     private final boolean success;
     private final T value;
 
-    TypeAdapterResult() {
+    public TypeAdapterResult() {
         this.success = false;
         this.value = null;
     }
 
-    TypeAdapterResult(final T value) {
+    public TypeAdapterResult(final T value) {
         this.success = true;
         this.value = value;
     }
