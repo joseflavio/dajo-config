ab49c0ffb69258510a8bba41c85a699c93413561
Jose Flavio <joseflavio@gmail.com>
2013-03-11 12:17:12 +0100

    improving build system

---
+++ dajo-config/dajo-config/src/deprecated/org/dajo/framework/filepath/DajoFilepath.java
@@ -0,0 +1,31 @@
+package org.dajo.framework.filepath;
+
+
+public final class DajoFilepath {
+
+    private final Type type;
+    private final String filepath;
+
+    public DajoFilepath(final Type type, final String filepath) {
+        this.type = type;
+        this.filepath = filepath;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public String getFilepath() {
+        return filepath;
+    }
+
+    @Override
+    public String toString() {
+        return "FilepathVO [type=" + type + ", filepath=" + filepath + "]";
+    }
+
+    public static enum Type {
+        CLASS_PATH, FILESYSTEM
+    }
+
+}// class
